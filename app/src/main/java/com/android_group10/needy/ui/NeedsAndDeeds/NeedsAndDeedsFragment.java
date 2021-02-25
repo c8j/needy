4f07a0f33576581447346e982573f88389af4edd
liliia <liliia.allansson0085@stud.hkr.se>
2021-02-25 17:00:12 +0100

    started work on Needs&Deeds

--- app/src/main/java/com/android_group10/needy/ui/NeedsAndDeeds/NeedsAndDeedsFragment.java
+++ app/src/main/java/com/android_group10/needy/ui/NeedsAndDeeds/NeedsAndDeedsFragment.java
@@ -9,27 +9,55 @@ import android.widget.TextView;
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
 import androidx.lifecycle.Observer;
 import androidx.lifecycle.ViewModelProvider;
+import androidx.recyclerview.widget.DefaultItemAnimator;
+import androidx.recyclerview.widget.GridLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
+import com.android_group10.needy.Post;
+import com.android_group10.needy.PostAdapter;
 import com.android_group10.needy.R;
+import com.android_group10.needy.ui.InNeed.OpenPostRecordFragment;
 
-public class NeedsAndDeedsFragment extends Fragment {
+import java.util.ArrayList;
+
+public class NeedsAndDeedsFragment extends Fragment implements PostAdapter.OnItemClickListener{
 
     private NeedsAndDeedsViewModel needsAndDeedsViewModel;
+    private static ArrayList<Post> dataList = new ArrayList<>();
+    private PostAdapter myPostAdapter;
 
     public View onCreateView(@NonNull LayoutInflater inflater,
                              ViewGroup container, Bundle savedInstanceState) {
         needsAndDeedsViewModel =
                 new ViewModelProvider(this).get(NeedsAndDeedsViewModel.class);
         View root = inflater.inflate(R.layout.fragment_needs_and_deeds, container, false);
-        final TextView textView = root.findViewById(R.id.text_needs_and_deeds);
-        needsAndDeedsViewModel.getText().observe(getViewLifecycleOwner(), new Observer<String>() {
+        final RecyclerView recyclerView = root.findViewById(R.id.postRecyclerView_needs_and_deeds);
+
+        myPostAdapter = new PostAdapter(dataList, this);
+
+        needsAndDeedsViewModel.getList().observe(getViewLifecycleOwner(), new Observer<ArrayList>() {
             @Override
-            public void onChanged(@Nullable String s) {
-                textView.setText("This is Needs and Deeds Fragment");
+            public void onChanged(@Nullable ArrayList s) {
+                recyclerView.setHasFixedSize(true);
+                recyclerView.setAdapter(myPostAdapter);
+                RecyclerView.LayoutManager lm = new GridLayoutManager(getContext(), 1);
+                recyclerView.setLayoutManager(lm);
+                recyclerView.setItemAnimator(new DefaultItemAnimator());
             }
         });
         return root;
+    }
+
+    @Override
+    public void onItemClick(int position) {
+        Post clickedItem = dataList.get(position);
+        FragmentManager fm = getChildFragmentManager();
+        FragmentTransaction fragmentTransaction = fm.beginTransaction();
+        fragmentTransaction.replace(R.id.layout_in_need_fragment, new OpenPostRecordFragment(clickedItem));
+        fragmentTransaction.commit();
     }
 }
