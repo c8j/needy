2aa45d81c1da614c33576f51d28fa0481b256e12
Calin <23583666+c8j@users.noreply.github.com>
2021-03-01 19:11:44 +0100

    Refactored Request data class Refactored Conversation data class Added FirestoreUtil singleton to handle firestore transactions

--- app/src/main/java/com/android_group10/needy/ui/messaging/data/Conversation.kt
+++ app/src/main/java/com/android_group10/needy/ui/messaging/data/Conversation.kt
@@ -1,9 +1,6 @@
 package com.android_group10.needy.ui.messaging.data
 
-import com.android_group10.needy.Post
-import com.android_group10.needy.User
-
 data class Conversation(
-    val associatedPostUID: String,
-    val partnerUID: String
+    val partnerUID: String,
+    val latestMessage: String
 )
--- app/src/main/java/com/android_group10/needy/ui/messaging/data/Request.kt
+++ app/src/main/java/com/android_group10/needy/ui/messaging/data/Request.kt
@@ -1,9 +1,6 @@
 package com.android_group10.needy.ui.messaging.data
 
-import com.android_group10.needy.Post
-import com.android_group10.needy.User
-
 data class Request(
     val associatedPostUID: String,
-    val communicationPartnerUID: String
+    val senderUID: String
 )
